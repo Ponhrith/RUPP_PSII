5f5f91536ad43afd89b242c3d0f0aff06d3475d2
Ponhrith <ponhrith.nhep@gmail.com>
2023-01-12 21:06:23 +0700

    Testing

--- median.py
+++ median.py
@@ -1,9 +1,24 @@
-ls = [0, 0, 0, 0, 0, 0, 3, 4, 4, 4, 5, 5, 7, 7, 8, 8, 8, 12, 12, 13, 13, 13, 14, 14, 16, 18, 19, 19, 20, 20, 21, 22, 23, 
-     26, 36, 36, 37, 42, 84]
-lst = sorted(ls)
-n = len(lst)
-if n%2:
-    print(lst[n//2])
-else:
-    print((lst[(n//2)-1]+lst[(n//2)]))
+# ls = [0, 0, 0, 0, 0, 0, 3, 4, 4, 4, 5, 5, 7, 7, 8, 8, 8, 12, 12, 13, 13, 13, 14, 14, 16, 18, 19, 19, 20, 20, 21, 22, 23, 
+#      26, 36, 36, 37, 42, 84]
+# lst = sorted(ls)
+# n = len(lst)
+# if n%2:
+#     print(lst[n//2])
+# else:
+#     print((lst[(n//2)-1]+lst[(n//2)]))
 
+# def Subtraction(num1, num2):
+#     sub = num1 - num2
+#     print("Subtraction:", sub)
+
+# Subtraction(5,3)
+
+# def Multiply(num1, num2, num3):
+#     mul = num1 * num2 * num3
+#     print("Multiply:", mul)
+
+# Multiply(1,2,3)
+
+list = ["I", "love", "Python", "so", "much!"]
+list[1] = "hate"
+print(list)
